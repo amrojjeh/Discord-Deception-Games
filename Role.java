c6fa82582c2c7d52ae1585d3750af63dfc14e6bf
alexman37 <62260530+alexman37@users.noreply.github.com>
2020-03-16 16:00:06 -0400

    epstein didnt kill himself

--- Role.java
+++ Role.java
@@ -1,14 +1,43 @@
 public interface Role{
+	/*
 	//what role is it? (extension)
 	//role name
+
 	//faction
+	//faction name
+
 	//priority
+
 	//attack stat, defense stat
+
 	//overriden method execute()
+
 	//role block immunity
 	//control immunity
+
 	//how can executioner become a jester
-	//jester that already won
-	//dead role that can only talk to dead and mediums
-	
+
+	//special attributes
+
+	//a "dead" role that can only talk to dead and mediums
+	*/
+
+
+	String getRoleName();
+
+	String getFactionName();
+
+	int getPriority();
+
+	//return attack stat
+	//return defense stat
+
+	boolean execute();
+
+	boolean hasRBImunnity();
+
+	boolean hasControlImmunity();
+
+	//event listener for attributes
+
 }
