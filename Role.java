823c3c61bd0dd1ea8260704328169aaa877ce471
alexman37 <62260530+alexman37@users.noreply.github.com>
2020-03-16 15:43:20 -0400

    Hello

---
+++ Role.java
@@ -0,0 +1,14 @@
+public interface Role{
+	//what role is it? (extension)
+	//role name
+	//faction
+	//priority
+	//attack stat, defense stat
+	//overriden method execute()
+	//role block immunity
+	//control immunity
+	//how can executioner become a jester
+	//jester that already won
+	//dead role that can only talk to dead and mediums
+	
+}
