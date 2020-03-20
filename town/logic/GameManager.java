4c465202724573914ce0e7a9760bad7bc984a102
amrojjeh <amrojjeh@gmail.com>
2020-03-19 20:35:26 -0500

    I was supposed to push this last time but forgot, oops

--- town/logic/GameManager.java
+++ town/logic/GameManager.java
@@ -3,7 +3,6 @@ package town.logic;
 import java.util.ArrayList;
 import town.logic.phases.*;
 import town.logic.delegates.ActionOne;
-import town.logic.roles.*;
 
 public class GameManager
 {
@@ -31,7 +30,9 @@ public class GameManager
 		while (running)
 		{
 			Event event = null;
-			while (gameInterface.pollEvent(event))
+
+			// It's assumed that if event is not assigned by polLEvent, then it should return false
+			for (event = gameInterface.pollEvent(); event != null; event = gameInterface.pollEvent())
 			{
 				switch (event.name)
 				{
@@ -47,6 +48,7 @@ public class GameManager
 					default: break;
 				}
 			}
+			
 			if (currentPhase.isOver())
 			{
 				switch (currentPhase.getName())
@@ -54,8 +56,8 @@ public class GameManager
 					case "Day": break;
 					case "Trial": break;
 					case "Night":
-						actionQueue.sort((one, two) -> one.compareTo(two));
-						executeActionQueueBasedOnEvent((action) -> action.actor.getRole().execute(action.actor, action.target));
+						sortActions();
+						executeActionQueueBasedOnEvent((action) -> action.actor.visit(action.target));
 						break;
 					default: break;
 				}
@@ -80,67 +82,19 @@ public class GameManager
 		actionQueue.add(action);
 	}
 
-	// JUST FOR TESTING
-	static void addAction(PlayerAction action, ArrayList<PlayerAction> actionQueue)
-	{
-		// In case there's a duplicate
-		// if (!action.actor.getRole().canExecute(action.actor, action.target)) return;
-		actionQueue.remove(action);
-		actionQueue.add(action);
-	}
-
 	void executeActionQueueBasedOnEvent(ActionOne<PlayerAction> event)
 	{
 		for (PlayerAction action : actionQueue)
 			event.run(action);
 	}
 
-	public static void main(String[] args)
+	public void sortActions()
 	{
-		ArrayList<PlayerAction> actions = new ArrayList<>();
-		// Test actionQueue
-		for (int x = 0; x < 10; ++x)
-		{
-			Role role = null;
-			int r = (int)Math.floor(Math.random() * 8);
-			switch (r)
-			{
-				case 0:
-					role = new Bodyguard();
-					break;
-				case 1:
-					role = new Doctor();
-					break;
-				case 2:
-					role = new Investigator();
-					break;
-				case 3:
-					role = new Sheriff();
-					break;
-				case 4:
-					role = new SuicideVigilante();
-					break;
-				case 5:
-					role = new VampireHunter();
-					break;
-				case 6:
-					role = new Veteran();
-					break;
-				case 7:
-					role = new Vigilante();
-					break;
-				default: role = new Doctor(); break;
-			}
-			Player p = new Player(x, x + "", role);
-			PlayerAction action = new PlayerAction(p, p);
-			System.out.println(role);
-			addAction(action, actions);
-		}
-		actions.sort((one, two) -> one.compareTo(two));
-		for (PlayerAction action : actions)
-			System.out.println("Role: " + action.actor.getRole().getRoleName());
+		actionQueue.sort((one, two) -> one.compareTo(two));
 	}
 }
+
+
 
 class PlayerAction
 {
