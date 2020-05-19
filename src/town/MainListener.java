4450895210994f3def0bc728276fd8cfcc05e924
amrojjeh <amrojjeh@gmail.com>
2020-05-19 11:35:52 -0500

    Added callback

--- src/town/MainListener.java
+++ src/town/MainListener.java
@@ -3,37 +3,59 @@ package town;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 
 public class MainListener extends ListenerAdapter
 {
 	public static void main(String[] args)
 	{
+		String token;
+		JDA jda;
 		try
 		{
-			System.out.println(loadToken());
-			// JDA jda = new JDABuilder();
+			token = loadToken();
 		}
-		catch(Exception e)
+		catch (FileNotFoundException e) 
 		{
-			System.out.println(e);
+			System.out.println("token.txt did not exist.");
 			return;
 		}
-
+		
+		if (token == "")
+			System.out.println("No token is found in token.txt");
+		
+		try 
+		{
+			jda = new JDABuilder(token).addEventListeners(new MainListener()).build();
+		}
+		catch (LoginException e) 
+		{
+			System.out.println("Couldn't login: " + e);
+			return;
+		}
 	}
 
+	public void onMessageReceived(MessageReceivedEvent e)
+	{
+		// Put your code here to react to message
+	}
+	
 	public static String loadToken()
 		throws FileNotFoundException
 	{
 		File file = new File("token.txt");
-		if (!file.exists()) throw new FileNotFoundException("token.txt was not found");
 		Scanner scanner = new Scanner(file);
-		if (!scanner.hasNextLine()) throw new java.util.NoSuchElementException("Empty file, please insert the token");
+		if (!scanner.hasNextLine())
+		{
+			scanner.close();
+			return "";
+		}
 		String token = scanner.nextLine();
 		scanner.close();
 		return token;
