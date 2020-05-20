04bcfd35b8439cc47b116d672452e2f2ae1f9a40
amrojjeh <amrojjeh@gmail.com>
2020-05-19 20:06:36 -0500

    Forgot to commit

--- src/town/Accusation.java
+++ src/town/Accusation.java
@@ -2,31 +2,32 @@ package town;
 
 public class Accusation extends Phase
 {
-	public Accusation()
+	public Accusation(PhaseManager pm) 
 	{
-		
+		super(pm);
 	}
 	
 	@Override
-	public void start() {
-		
-	}
-	
-	@Override
-	public void end() {
-		
-	}
-	
-	@Override
-	public void run()
+	public void start() 
 	{
-		Timer timer = new Timer("Phase Timer");
+		System.out.println("Starting accusation...");
 	}
-
+	
 	@Override
-	public int duration() {
+	public void end() 
+	{
+		System.out.println("Ending accusation...");
+	}
+	
+	@Override
+	public Phase getNextPhase(PhaseManager pm)
+	{
+		return new Day(pm); // TODO: Replace with the actual next phase
+	}
+	
+	@Override
+	public int getDuration()
+	{
 		return 3000;
 	}
-	
-
 }
