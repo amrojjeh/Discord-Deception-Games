b13e133362c7f79c1dac825c474d1811bd426189
dinglydo <amrojjeh@gmail.com>
2020-10-16 18:55:48 -0500

    Added docs

--- src/town/persons/Attributes.java
+++ src/town/persons/Attributes.java
@@ -5,6 +5,11 @@ public class Attributes
 	final AttributeValue attack;
 	final AttributeValue defense;
 
+	/**
+	 * Construct a new Attributes class. Used by roles to denote defense and attack.
+	 * @param defense Set the defense
+	 * @param attack Set the attack
+	 */
 	public Attributes(AttributeValue defense, AttributeValue attack)
 	{
 		this.defense = defense;
