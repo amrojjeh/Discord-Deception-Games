df728a7e799e2b915ccb4b0d0c9713bd8ee1e8fe
dinglydo <amrojjeh@gmail.com>
2020-10-16 18:55:03 -0500

    Refactored Lookout

--- src/town/mafia/roles/Lookout.java
+++ src/town/mafia/roles/Lookout.java
@@ -1,30 +1,35 @@
-package town.mafia.persons;
+package town.mafia.roles;
 
 import java.util.List;
 
-import town.DiscordGame;
+import javax.annotation.Nonnull;
+
 import town.events.LookoutTownEvent;
 import town.mafia.phases.Night;
+import town.persons.AttributeValue;
+import town.persons.Attributes;
 import town.persons.Person;
-import town.roles.GameRole;
+import town.roles.EmptyRoleData;
+import town.roles.Faction;
+import town.roles.Role;
+import town.roles.RoleData;
 
-public class Lookout extends Person
+public class Lookout implements Role
 {
-	public Lookout(DiscordGame game, int num, Long id)
-	{
-		super(game, num, id, GameRole.LOOKOUT);
-	}
+	public final Attributes attr = new Attributes(AttributeValue.NONE, AttributeValue.NONE);
+	public final String name = "Lookout";
 
 	@Override
-	public String ability(List<Person> references)
+	@Nonnull
+	public String ability(Person user, List<Person> references)
 	{
-		if (!isAlive())
+		if (!user.isAlive())
 			return "Can't watch people if you're dead.";
 		if (references.isEmpty())
 			return "There's no person to watch. `!ability 1` to watch the first person shown in `!party`.";
 		if (references.size() > 1)
 			return "Cannot watch more than one person at once. `!ability 1` to watch the first person show in `!party`.";
-		if (!(getGame().getCurrentPhase() instanceof Night))
+		if (!(user.getGame().getCurrentPhase() instanceof Night))
 			return "Lookouts can only watch visitors during the night.";
 		if (!references.get(0).isAlive())
 			return "Lookouts can't watch dead people.";
@@ -34,29 +39,58 @@ public class Lookout extends Person
 
 		String msg = "";
 
-		if (event != null)
+		if (user.getTownEvent() != null)
 		{
 			msg += "You've changed your mind.\n";
-			cancel();
+			user.clearTownEvent();
 		}
 
-		event = new LookoutTownEvent(getGame(), this, references.get(0));
-		getGame().addEvent(event);
+		user.setTownEvent(new LookoutTownEvent(user.getGame(), user, references.get(0)));
+		user.getGame().addEvent(user.getTownEvent());
 
 		return msg + String.format("You will watch <@%d> tonight.", references.get(0).getID());
 	}
 
 	@Override
-	public List<Person> getPossibleTargets()
+	public List<Person> getPossibleTargets(Person user)
 	{
-		return getGame().getAlivePlayers();
+		return user.getGame().getAlivePlayers();
 	}
 
 	@Override
+	@Nonnull
 	public String getHelp()
 	{
 		return "LOOKOUT\n" +
 				"Lookouts win with other townees. His goal is to eliminate any evil doer.\n" +
 				"Ability: Can see who visited a person that night. Ex: `!ability 2` watches person number two. Check a person's number with !party";
+	}
+
+	@Override
+	@Nonnull
+	public Attributes getAttributes()
+	{
+		return attr;
+	}
+
+	@Override
+	@Nonnull
+	public String getName()
+	{
+		return name;
+	}
+
+	@Override
+	@Nonnull
+	public Faction getFaction()
+	{
+		return Faction.TOWN;
+	}
+
+	@Override
+	@Nonnull
+	public RoleData getInitialRoleData()
+	{
+		return new EmptyRoleData();
 	}
 }
